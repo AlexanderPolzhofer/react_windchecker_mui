4a7c02fdd9a18f9a33ed095b0902d9b3f9ff6f16
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-11-01 16:56:26 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Component } from "react";
 import './App.css';
-import Button from '@mui/material/Button';
+import SpotFetch from "./components/SpotFetch";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {}
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Button variant="contained">Hello</Button>
-        </header>
+      
+          <SpotFetch />
+       
       </div>
     );
   }
