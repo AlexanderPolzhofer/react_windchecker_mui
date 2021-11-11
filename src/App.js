e37c5d98d3694a1823c4748c0a1ed5aa6ee5f0ab
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-11-11 22:01:21 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,27 +4,56 @@ import IFrameWidget from './components/IFrameWidget';
 import Navbar from './components/Navbar';
 import Spotlist from './components/Spotlist';
 
-
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      isWeatherVisible: false,
       spots: [],
-      widgets: [{
-        "widget":
-          <IFrameWidget />
-      }]
+      widgets: []
     };
+  }
+
+  setWeatherVisible = () => {
+    this.setState({ isWeatherVisible: true });
+    this.checkVisibility();
+  }
+
+  createWidgetObject = () => {
+    let widgets = this.state.widgets;
+    let id = 1;
+    widgets.push(<IFrameWidget url="https://wetter.orf.at/vorarlberg/" id={id} />);
+    this.setState({ widgets: widgets });
+  }
+
+  checkVisibility = () => {
+    if (this.state.isWeatherVisible === true) {
+      this.createWidgetObject();
+    }
+
+  }
+
+  deleteWidget = () => {
+    let index = this.state.widgets.indexOf(1);
+    if (index > 0) {
+      index.splice(0);
+    }
   }
 
   render() {
     return (
       <div className="App">
-        <Navbar weather={this.state.widgets.widget}/>
+        <Navbar weatherStatus={() => this.setWeatherVisible()} />
         <Spotlist />
-
+        <h1>visible: {this.state.isWeatherVisible.toString()} </h1>
+        {this.state.widgets.map((widget) => (
+          <div key="1">{widget}</div>
+        ))
+        }
       </div>
+
+
     )
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,23 +8,20 @@ import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 class Navbar extends React.Component {
     constructor(props) {
         super(props);
+        // this.setWeatherVisible = this.setWeatherVisible.bind(this);
         this.state = {}
     }
 
-    urlORFWeather = () => {
-        return "https://wetter.orf.at/vorarlberg/";
-    }
+
 
     getAllSpots = () => {
         alert("clicked");
     }
 
-    getWeatherFromORF = () => {
-        <iframe title="titleORF" >{this.urlORFWeather}</iframe>
-    }
 
-    doAlert = () => {
-        alert("doit:");
+
+    getWeatherStatusFromNavbar = () => {
+        this.props.weatherStatus();
     }
 
     render() {
@@ -33,7 +30,7 @@ class Navbar extends React.Component {
             <h1>WINDCHECKER</h1>
             <div >
                 <PhotoCameraIcon className={NavbarStyle.infoIcons} onClick={() => { alert("clicked") }} />
-                <CloudIcon className={NavbarStyle.infoIcons} onClick={this.doAlert} />
+                <CloudIcon className={NavbarStyle.infoIcons} onClick={this.getWeatherStatusFromNavbar} />
             </div>
         </div>);
     }
